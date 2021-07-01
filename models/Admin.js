d9117eafb1493acda56b61fadb0fc66ad135055f
kiss8981 <kdh89811@gmail.com>
2021-07-01 19:10:17 +0900

    [FIX] admin 모델 최적화

--- models/Admin.js
+++ models/Admin.js
@@ -1,16 +1,14 @@
-const mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+    const mongoose = require('mongoose');
+    var Schema = mongoose.Schema;
 
+    var adminSchema = new Schema({
+        date: String,
+        msg: String,
+        author: String,
+        title: String,
+        published_date: { type: Date, default: Date.now }
+    }, {collection: 'adminAlart'});
 
+    var Admin = mongoose.model('adminAlart', adminSchema);
 
-var adminSchema = new Schema({
-    date: String,
-    msg: String,
-    author: String,
-    title: String,
-    published_date: { type: Date, default: Date.now }
-}, {collection: 'adminAlart'});
-
-var Admin = mongoose.model('adminAlart', adminSchema);
-
-module.exports = { Admin }
+    module.exports = { Admin }
